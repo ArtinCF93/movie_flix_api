9fb85ff2d024d71ebd26cd6e23f7d7394cd78b7c
ArtinCF93 <Toojool@gmail.com>
2021-10-16 10:08:30 -0700

    Cors Policy Upate
    Simplified

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ let cors = require('cors');
 let allowedOrigins = ['http://localhost:3000', 'https://quiet-headland-10477.herokuapp.com/', 'http://localhost:1234', 'https://en.wikipedia.org'];
 
 app.use(cors(
-  {
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}
+//  {
+//  origin: (origin, callback) => {
+//    if(!origin) return callback(null, true);
+//    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//      return callback(new Error(message ), false);
+//    }
+//    return callback(null, true);
+//  }
+//}
+
 ));
 
 app.use(express.json());
