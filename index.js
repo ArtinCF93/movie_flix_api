9e58262a15734438754a9cc0fa4e45cd66de5c87
ArtinCF93 <Toojool@gmail.com>
2022-07-04 20:44:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ let mongoose = require('mongoose');
 // const bcrypt = require('bcrypt');
 
 // Cross-Origin Resource Sharing
-let cors = require('cors');
+const cors = require('cors');
+app.use(cors({ origin: true }));
+// let cors = require('cors');
 // let allowedOrigins = [
 //   "http://localhost:8080",
 //   "http://localhost:3000",
@@ -23,20 +25,20 @@ let cors = require('cors');
 //   "https://artincf93.github.io",
 // ];
 
-app.use(
-  cors(
-  //   {
-  //   origin: (origin, callback) => {
-  //     if (!origin) return callback(null, true);
-  //     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-  //       let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-  //      return callback(new Error(message ), false);
-  //    }
-  //    return callback(null, true);
-  //  }
-  // }
-  )
-);
+// app.use(
+//   cors(
+//     {
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//         let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//        return callback(new Error(message ), false);
+//      }
+//      return callback(null, true);
+//    }
+//   }
+//   )
+// );
 
 app.use(express.json());
 
