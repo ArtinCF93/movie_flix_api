e836759b7fc4d715c6d79488e9d2115fa116f284
ArtinCF93 <Toojool@gmail.com>
2022-07-04 20:10:08 -0700

    editing cors

--- index.js
+++ index.js
@@ -13,28 +13,28 @@ let mongoose = require('mongoose');
 // const bcrypt = require('bcrypt');
 
 // Cross-Origin Resource Sharing
-let cors = require('cors');
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://localhost:3000",
-  "http://localhost:4200",
-  "http://localhost:1234",
-  "https://mvflix.netlify.app",
-  "https://artincf93.github.io",
-];
+// let cors = require('cors');
+// let allowedOrigins = [
+//   "http://localhost:8080",
+//   "http://localhost:3000",
+//   "http://localhost:4200",
+//   "http://localhost:1234",
+//   "https://mvflix.netlify.app",
+//   "https://artincf93.github.io",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-        let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-       return callback(new Error(message ), false);
-     }
-     return callback(null, true);
-   }
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//         let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//        return callback(new Error(message ), false);
+//      }
+//      return callback(null, true);
+//    }
+//   })
+// );
 
 app.use(express.json());
 
